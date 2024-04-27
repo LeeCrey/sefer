a8fc731bf6b602f6ddf75ae69057b2efceb5bdee
Solomon Boloshe <solomonboloshe11x@gmail.com>
2024-04-27 16:35:26 +0300

    renamed

--- app/src/main/java/com/social/sefer/ui/vms/HomeVM.kt
+++ app/src/main/java/com/social/sefer/ui/vms/HomeVM.kt
@@ -1,10 +1,10 @@
-package com.social.sefer.ui.home
+package com.social.sefer.ui.vms
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class HomeVM : ViewModel() {
 
   private val _text = MutableLiveData<String>().apply {
     value = "This is home Fragment"
