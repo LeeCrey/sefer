1851656d3856f0cda256b050d882ee28d7b2b176
Solomon Boloshe <solomonboloshe11x@gmail.com>
2024-04-27 16:31:28 +0300

    added fields

--- app/src/main/java/com/social/sefer/Constants.kt
+++ app/src/main/java/com/social/sefer/Constants.kt
@@ -1,4 +1,14 @@
 package com.social.sefer
 
+import android.content.Context
+
 object Constants {
+  const val PREFS_MODE = Context.MODE_PRIVATE
+  const val PREFS_FILE = "com.social.sefer.sefer_pref"
+  const val AUTHORIZATION = "com.social.sefer.auth_token"
+  const val USERID = "com.social.sefer.user_id"
+  const val LANG = "com.social.sefer.language"
+  const val SYSTEM: String = "system"
+  const val THEME: String = "theme"
+  const val DARK: String = "dark"
 }
