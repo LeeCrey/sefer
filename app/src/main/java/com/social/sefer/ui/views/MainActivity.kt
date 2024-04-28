a738913ef3e1f81cb08e1b716dc622999d15b3aa
Solomon Boloshe <solomonboloshe11x@gmail.com>
2024-04-28 13:58:45 +0300

    back press registered

--- app/src/main/java/com/social/sefer/ui/views/MainActivity.kt
+++ app/src/main/java/com/social/sefer/ui/views/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.social.sefer.ui.views
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,6 +20,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.social.sefer.R
 import com.social.sefer.databinding.ActivityMainBinding
+import com.social.sefer.utilities.ApplicationHelper
 import com.social.sefer.utilities.ViewUtils
 
 
@@ -23,6 +30,8 @@ class MainActivity : MyAppActivity() {
   private lateinit var binding: ActivityMainBinding
   private lateinit var navController: NavController
   private lateinit var bottomNav: BottomNavigationView
+  private lateinit var backPressCB: OnBackPressedCallback
+  private lateinit var drawerLayout: DrawerLayout
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -33,7 +42,7 @@ class MainActivity : MyAppActivity() {
     val toolbar: Toolbar = findViewById(R.id.tool_bar)
     setSupportActionBar(toolbar)
 
-    val drawerLayout: DrawerLayout = binding.drawerLayout
+    drawerLayout = binding.drawerLayout
     val navView: NavigationView = binding.navView
 
     val navHostFragment =
@@ -54,10 +63,76 @@ class MainActivity : MyAppActivity() {
     navView.setBackgroundColor(surface)
     bottomNav.setBackgroundColor(surface)
 
+
+    navView.setNavigationItemSelectedListener {
+      when (val id = it.itemId) {
+        R.id.menu_item_rate_app -> rateApp()
+        R.id.menu_item_share -> shareApp()
+        R.id.nav_settings -> {
+          navController.navigate(id, null, ApplicationHelper.slideAnimation)
+        }
+
+        else -> onNavDestinationSelected(it, navController)
+      }
+      drawerLayout.close()
+
+      false
+    }
+
+    drawerLayout.addDrawerListener(object : DrawerLayout.SimpleDrawerListener() {
+      override fun onDrawerOpened(drawerView: View) {
+        super.onDrawerOpened(drawerView)
+
+        backPressCB.isEnabled = true
+      }
+
+      override fun onDrawerClosed(drawerView: View) {
+        super.onDrawerClosed(drawerView)
+
+        backPressCB.isEnabled = false
+      }
+    })
+    registerBackPress()
     ViewUtils.setupSystemBars(this, isDarkMode())
   }
 
   override fun onSupportNavigateUp(): Boolean {
     return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+  }
+
+  private fun registerBackPress() {
+    backPressCB = object : OnBackPressedCallback(false) {
+      override fun handleOnBackPressed() {
+        drawerLayout.close()
+      }
+    }
+    onBackPressedDispatcher.addCallback(backPressCB)
+  }
+
+  private fun rateApp() {
+    try {
+      startActivity(
+        Intent(
+          Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")
+        )
+      )
+    } catch (_: ActivityNotFoundException) {
+      startActivity(
+        Intent(
+          Intent.ACTION_VIEW,
+          Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+        )
+      )
+    }
+  }
+
+  private fun shareApp() {
+    val appUri = "https://play.google.com/store/apps/details?id=$packageName"
+    val sendIntent = Intent().apply {
+      action = Intent.ACTION_SEND
+      type = "text/plain"
+      putExtra(Intent.EXTRA_TEXT, appUri)
+    }
+    startActivity(Intent.createChooser(sendIntent, resources.getText(R.string.send_to)))
   }
 }
